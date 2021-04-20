1dea1d8454070b21ffa3b40bed181ffc5d062305
David Conneely <dconneely@users.noreply.github.com>
2021-04-20 22:43:02 +0100

    Update src/main/java/hudson/plugins/dimensionsscm/DimensionsChangeLogParser.java

--- src/main/java/hudson/plugins/dimensionsscm/DimensionsChangeLogParser.java
+++ src/main/java/hudson/plugins/dimensionsscm/DimensionsChangeLogParser.java
@@ -71,7 +71,7 @@ public class DimensionsChangeLogParser extends ChangeLogParser {
                 digester.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
             }
             catch ( ParserConfigurationException ex) {
-                throw new SAXException("Failed to securely configure CVS changelog parser", ex);
+                throw new SAXException("Failed to securely configure Dimensions changelog parser", ex);
             }
         }
         digester.push(top);
